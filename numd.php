6f87ffe2d3c43b165f1437ca4c7bb475fb0d9382
andrepolischuk <andre.polischuk@gmail.com>
2014-11-06 12:34:50 +0300

    Refactor tabs

--- numd.php
+++ numd.php
@@ -1,65 +1,65 @@
 <?php
 
-	class Numd {
+  class Numd {
 
-		/**
-		 * Decline value
-		 * @param  number $num
-		 * @param  array  $word
-		 * @return string
-		 */
+    /**
+     * Decline value
+     * @param  number $num
+     * @param  array  $word
+     * @return string
+     */
 
-		private static function decline($num, $word) {
+    private static function decline($num, $word) {
 
-			$num = abs($num);
+      $num = abs($num);
 
-	    // fractional
-	    if ((float) floor($num) !== $num) {
-	      return $word[1];
-	    }
+      // fractional
+      if ((float) floor($num) !== $num) {
+        return $word[1];
+      }
 
-	    // integer
-	    if ($num > 10 && (($num % 100) - (($num % 100) % 10)) / 10 === 1) {
-	      return $word[2];
-	    } else {
-	      $nn = $num % 10;
-	      return $word[($nn === 0 || $nn >= 5) ? 2 : ($nn >= 2 ? 1 : 0)];
-	    }
+      // integer
+      if ($num > 10 && (($num % 100) - (($num % 100) % 10)) / 10 === 1) {
+        return $word[2];
+      } else {
+        $nn = $num % 10;
+        return $word[($nn === 0 || $nn >= 5) ? 2 : ($nn >= 2 ? 1 : 0)];
+      }
 
-		}
+    }
 
-		/**
-	   * Numd call
-	   * @param  array $arguments
-	   * @return string
-	   */
+    /**
+     * Numd call
+     * @param  array $arguments
+     * @return string
+     */
 
-		public static function call($arguments) {
+    public static function call($arguments) {
 
-			$num  = (float) $arguments[0];
-			$word = $arguments;
+      $num  = (float) $arguments[0];
+      $word = $arguments;
 
-			array_splice($word, 0, 1);
+      array_splice($word, 0, 1);
 
-			if (count($word) === 3) {
-				return $num . ' ' . self::decline($num, $word);
-			}
+      if (count($word) === 3) {
+        return $num . ' ' . self::decline($num, $word);
+      }
 
-		}
+    }
 
-	}
+  }
 
-	/**
-	 * Numd wrapper
-	 * @param  number $num
-	 * @param  string $nominative
-	 * @param  string $genitiveSingular
-	 * @param  string $genitivePlural
-	 * @return string
-	 */
+  /**
+   * Numd wrapper
+   * @param  number $num
+   * @param  string $nominative
+   * @param  string $genitiveSingular
+   * @param  string $genitivePlural
+   * @return string
+   */
 
-	function numd() {
-		return Numd::call(func_get_args());
-	}
+  function numd() {
+    return Numd::call(func_get_args());
+  }
 
 ?>
